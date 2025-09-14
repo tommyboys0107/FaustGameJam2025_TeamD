333606fdea8b4e2f863040d416de1116d01d6103
Goo <kuwoods@hotmail.com>
2025-09-14 10:26:28 +0800

    Add Menu scene

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -1,11 +1,12 @@
 using HideAndSeek.Core;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
     public void OnStartClick()
     {
-        GameManager.Instance.StartGame();
+        SceneManager.LoadScene(1);
     }
 
     public void OnExitClick()
