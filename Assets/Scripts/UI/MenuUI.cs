a2a37612c44e24ff647a1fff2583d0538f74ddc7
Goo <kuwoods@hotmail.com>
2025-09-14 13:13:06 +0800

    Import BGM and audio play

--- Assets/Scripts/UI/MenuUI.cs
+++ Assets/Scripts/UI/MenuUI.cs
@@ -6,11 +6,13 @@ public class MenuUI : MonoBehaviour
 {
     public void OnStartClick()
     {
+        AudioEffectManaager.Instance.PlayUIClickEffect();
         SceneManager.LoadScene(1);
     }
 
     public void OnExitClick()
     {
+        AudioEffectManaager.Instance.PlayUIClickEffect();
         Application.Quit();
     }
 }
